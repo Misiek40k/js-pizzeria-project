7baea9899a4552ee4e853492e620115f091b60e4
Misiek40k <comanderrr@gmail.com>
2019-10-04 18:07:48 +0200

    Add classList adding and removing functionality in processOrder

--- src/js/script.js
+++ src/js/script.js
@@ -128,20 +128,33 @@
         processOrder() {
             const thisProduct = this;
 
+
             const formData = utils.serializeFormToObject(thisProduct.form);
             let price = thisProduct.data.price;
 
             if (thisProduct.data.params) {
                 Object.entries(thisProduct.data.params).forEach(function ([paramId, param]) {
 
+
                     for (let optionId in param.options) {
                         const option = param.options[optionId];
                         const optionSelected = formData.hasOwnProperty(paramId) && formData[paramId].indexOf(optionId) > -1;
+                        const images = thisProduct.imageWrapper.querySelectorAll(`.${paramId}-${optionId}`);
 
                         if (optionSelected && !option.default) {
                             price += option.price;
                         } else if (!optionSelected && option.default) {
                             price -= option.price;
+                        }
+
+                        if (optionSelected) {
+                            for (let image of images) {
+                                image.classList.add('active');
+                            }
+                        } else {
+                            for (let image of images) {
+                                image.classList.remove('active');
+                            }
                         }
                     }
                 });
