da65e140a6645e7fa602c38670979eb9fcc95409
Misiek40k <comanderrr@gmail.com>
2019-10-05 20:07:17 +0200

    Add AmountWidget class and initAmountWidget method in Product class

--- src/js/script.js
+++ src/js/script.js
@@ -61,6 +61,7 @@
             thisProduct.getElements();
             thisProduct.initAccordion();
             thisProduct.initOrderForm();
+            thisProduct.initAmountWidget();
             thisProduct.processOrder();
 
             console.log('new product:', thisProduct);
@@ -84,6 +85,7 @@
             thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
             thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
             thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
+            thisProduct.amountWidgetElem = thisProduct.element.querySelector(select.menuProduct.amountWidget);
         }
 
         initAccordion() {
@@ -123,6 +125,12 @@
                 event.preventDefault();
                 thisProduct.processOrder();
             });
+        }
+
+        initAmountWidget() {
+            const thisProduct = this;
+
+            thisProduct.amountWidget = new AmountWidget(thisProduct.amountWidgetElem);
         }
 
         processOrder() {
@@ -186,6 +194,15 @@
             //     }
             // }
             thisProduct.priceElem.innerHTML = price;
+        }
+    }
+
+    class AmountWidget {
+        constructor(element){
+            const thisWidget = this;
+
+            console.log('AmountWidget: ', thisWidget);
+            console.log('constructor arguments: ', element);
         }
     }
 
