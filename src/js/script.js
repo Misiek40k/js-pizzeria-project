1bd4cf3deb424d1fffb2a46307a427c490205a62
Misiek40k <comanderrr@gmail.com>
2019-10-06 10:11:22 +0200

    Add cart toggle functionality in Cart class

--- src/js/script.js
+++ src/js/script.js
@@ -303,6 +303,7 @@
 
             thisCart.products = [];
             thisCart.getElements(element);
+            thisCart.initActions();
 
             console.log('new Cart: ', thisCart);
         }
@@ -312,6 +313,15 @@
 
             thisCart.dom = {};
             thisCart.dom.wrapper = element;
+            thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+        }
+
+        initActions() {
+            const thisCart = this;
+
+            thisCart.dom.toggleTrigger.addEventListener('click', function(){
+                thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+            });
         }
     }
 
