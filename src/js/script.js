9cbd4b07ca24192e1458cb07a4e6927db22ff1b8
Misiek40k <comanderrr@gmail.com>
2019-10-06 09:38:50 +0200

    Add class Cart in script.js

--- src/js/script.js
+++ src/js/script.js
@@ -297,6 +297,24 @@
         }
     }
 
+    class Cart {
+        constructor(element) {
+            const thisCart = this;
+
+            thisCart.products = [];
+            thisCart.getElements(element);
+
+            console.log('new Cart: ', thisCart);
+        }
+
+        getElements(element) {
+            const thisCart = this;
+
+            thisCart.dom = {};
+            thisCart.dom.wrapper = element;
+        }
+    }
+
     const app = {
         initMenu: function () {
             const thisApp = this;
@@ -314,6 +332,13 @@
             thisApp.data = dataSource;
         },
 
+        initCart: function () {
+            const thisApp = this;
+
+            const cartElement = document.querySelector(select.containerOf.cart);
+            thisApp.cart = new Cart(cartElement);
+        },
+
         init: function () {
             const thisApp = this;
             console.log('*** App starting ***');
@@ -324,6 +349,7 @@
 
             thisApp.initData();
             thisApp.initMenu();
+            thisApp.initCart();
         },
     };
 
