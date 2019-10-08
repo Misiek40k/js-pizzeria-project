29ff0e88d91a31c80f5b97cd0216469b4d3e6b5b
Misiek40k <comanderrr@gmail.com>
2019-10-08 20:35:45 +0200

    Add CartProduct class and getElements method

--- src/js/script.js
+++ src/js/script.js
@@ -359,6 +359,33 @@
         }
     }
 
+    class CartProduct {
+        constructor(menuProduct, element) {
+            const thisCartProduct = this;
+
+            thisCartProduct.id = menuProduct.id;
+            thisCartProduct.name = menuProduct.name;
+            thisCartProduct.price = menuProduct.price;
+            thisCartProduct.priceSingle = menuProduct.priceSingle;
+            thisCartProduct.amount = menuProduct.amount;
+            thisCartProduct.params = JSON.parse(JSON.stringify(menuProduct.params));
+
+            thisCartProduct.getElements(element);
+            console.log(thisCartProduct);
+        }
+
+        getElements(element) {
+            const thisCartProduct = this;
+
+            thisCartProduct.dom = {};
+            thisCartProduct.dom.wrapper = element;
+            thisCartProduct.dom.amountWidget = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.amountWidget);
+            thisCartProduct.dom.price = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.price);
+            thisCartProduct.dom.edit = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.edit);
+            thisCartProduct.dom.remove = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.remove);
+        }
+    }
+
     const app = {
         initMenu: function () {
             const thisApp = this;
