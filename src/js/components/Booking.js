bba125e2c874c110f173b3d8ac6bc3e5f5b59666
Misiek40k <comanderrr@gmail.com>
2019-10-16 22:19:21 +0200

    Add getData method to Booking class

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,4 +1,4 @@
-import { templates, select } from '../settings.js';
+import { templates, select, settings } from '../settings.js';
 import utils from '../utils.js';
 import AmountWidget from './AmountWidget.js';
 import DatePicker from './DatePicker.js';
@@ -10,6 +10,40 @@ class Booking {
 
         thisBooking.render(element);
         thisBooking.initWidgets();
+        thisBooking.getData();
+    }
+
+    getData() {
+        // const thisBooking = this;
+
+        const params = {
+            booking: [
+
+            ],
+            eventsCurrent: [
+
+            ],
+            eventsRepeat: [
+
+            ],
+        };
+
+        const urls = {
+            booking: new URL(
+                `${settings.db.booking}?${params.booking.join('&')}`,
+                `http://${settings.db.url}`
+            ),
+            eventsCurrent: new URL(
+                `${settings.db.event}?${params.eventsCurrent.join('&')}`,
+                `http://${settings.db.url}`
+            ),
+            eventsRepeat: new URL(
+                `${settings.db.event}?${params.eventsRepeat.join('&')}`,
+                `http://${settings.db.url}`
+            ),
+        };
+
+        console.log(urls);
     }
 
     initWidgets() {
