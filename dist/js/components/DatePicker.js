46a930ff788327f3fe8fc261ae224674385dc500
Misiek40k <comanderrr@gmail.com>
2019-10-21 21:24:23 +0200

    Add dist

---
+++ dist/js/components/DatePicker.js
@@ -0,0 +1,53 @@
+import { select, settings } from '../settings.js';
+import utils from '../utils.js';
+import BaseWidget from './BaseWidget.js';
+
+class DatePicker extends BaseWidget {
+    constructor(wrapper) {
+        super(wrapper, utils.dateToStr(new Date()));
+        const thisWidget = this;
+
+        thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.datePicker.input);
+        thisWidget.initPlugin();
+    }
+
+    initPlugin() {
+        const thisWidget = this;
+
+        thisWidget.minDate = new Date(thisWidget.value);
+        thisWidget.maxDate = utils.addDays(thisWidget.minDate, settings.datePicker.maxDaysInFuture);
+
+        flatpickr(thisWidget.dom.input, {
+            defaultDate: thisWidget.minDate,
+            minDate: thisWidget.minDate,
+            maxDate: thisWidget.maxDate,
+            'disable': [
+                function (date) {
+                    // return true to disable
+                    return (date.getDay() === 1);
+                }
+            ],
+            'locale': {
+                'firstDayOfWeek': 1 // start week on Monday
+            },
+            dateFormat: 'Y-m-d', // Displays: 2017-01-22Z
+            onChange: function( dateStr ) {
+                thisWidget.value = dateStr;
+            },
+        });
+    }
+
+    parseValue(value) {
+        return value;
+    }
+
+    isValid() {
+        return true;
+    }
+
+    renderValue() {
+
+    }
+}
+
+export default DatePicker;
